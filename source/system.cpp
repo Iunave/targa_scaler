1825f4f59bb018912a1a09b3667375af7cc11daf
Iunave <55002444+Iunave@users.noreply.github.com>
2023-09-15 22:29:08 +0200

    Add files via upload

--- source/system.cpp
+++ source/system.cpp
@@ -1,6 +1,7 @@
 #include "system.hpp"
 
 #if defined(__linux__)
+#warning "building for linux"
 
 #include <cerrno>
 #include <cstdio>
@@ -9,70 +10,65 @@
 #include <csignal>
 #include <sys/stat.h>
 
-class syscheck_t
+static void check_syscall(int ret, const char* info = nullptr)
 {
-public:
-    syscheck_t& operator=(__syscall_slong_t in_val)
+    if(ret == -1)
     {
-        val = in_val;
-        if(val == -1)
-        {
-            perror(nullptr);
-            abort();
-        }
-        return *this;
+        perror(info);
+        abort();
     }
-
-    operator __syscall_slong_t() const {return val;}
-    __syscall_slong_t val = 0;
-};
-inline constinit thread_local syscheck_t syscheck;
+}
 
 std::vector<uint8_t> read_file_binary(const char* filepath)
 {
-    syscheck = open(filepath, O_RDONLY);
-    int fd = syscheck;
+    int fd = open(filepath, O_RDONLY);
+    check_syscall(fd, filepath);
 
     struct stat64 statbuf;
-    syscheck = fstat64(fd, &statbuf);
+    check_syscall(fstat64(fd, &statbuf), filepath);
 
     std::vector<uint8_t> file_data(statbuf.st_size);
 
     size_t nread = read(fd, file_data.data(), file_data.size());
     if(nread != statbuf.st_size)
     {
-        perror(nullptr);
+        perror(filepath);
         abort();
     }
 
-    syscheck = close(fd);
+    check_syscall(close(fd), filepath);
 
     return file_data;
 }
 
 void write_file_binary(const char* filepath, std::span<uint8_t> data)
 {
-    syscheck = open(filepath, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
-    int fd = syscheck;
+    int fd = open(filepath, O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR);
+    check_syscall(fd, filepath);
 
     size_t nwrite = write(fd, data.data(), data.size());
     if(nwrite != data.size())
     {
-        perror(nullptr);
+        perror(filepath);
         abort();
     }
 
-    syscheck = close(fd);
-
+    check_syscall(close(fd), filepath);
 }
 
 #elif defined(__WIN32__)
+#warning "building for windows"
 
 #include <fstream>
 
 std::vector<uint8_t> read_file_binary(const char* filepath)
 {
     std::ifstream filestream{filepath, std::ios_base::in | std::ios_base::binary};
+    if(!filestream.is_open())
+    {
+        perror(filepath);
+        abort();
+    }
 
     std::vector<uint8_t> file_data{};
     while(!filestream.eof())
@@ -87,11 +83,18 @@ std::vector<uint8_t> read_file_binary(const char* filepath)
 
 void write_file_binary(const char* filepath, std::span<uint8_t> data)
 {
-    std::ifstream filestream{filepath, std::ios_base::out | std::ios_base::binary};
+    std::ofstream filestream{filepath, std::ios_base::out | std::ios_base::binary};
+    if(!filestream.is_open())
+    {
+        perror(filepath);
+        abort();
+    }
+
     for(uint8_t byte : data)
     {
-        filestream.putback(byte);
+        filestream.put(byte);
     }
+
     filestream.close();
 }
 
